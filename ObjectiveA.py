99ed9d0716711dd20a6127616233ac5b9db17f8c
capwrmt <cliff.c.chang@gmail.com>
2018-08-08 10:13:24 -0400

    updated plots

--- ObjectiveA.py
+++ ObjectiveA.py
@@ -1,37 +1,38 @@
 #!/usr/bin/env python
-# test new line
+
 import csv
-# Import `pyplot` from `matplotlib`
 import matplotlib.pyplot as plt
+from collections import namedtuple
 
 '''
 1. Read in CSV File into array 
 2. Calculate time to last byte (req time + transfer time + turnaround time)
-3. Group distribution into object size classes
+3. Group distribution into object size classes - algorithm / data structure
 4. Graph in a bar chart using pychart
 
 Todos:
 Error checking via Try-except
 '''
-
-f = open('exercise.log.csv')
+Msg = namedtuple('Msg', ('http_status', 'obj_sz', 'req_time', 'transfer_time', 'ts', 'turn_time'))
 
 data = []
+
 count = 0 # Count total number of rows in CSV
+with open('exercise.log.csv') as csv_file:
+	reader = csv.reader(csv_file)
+	for row in reader:
+		data.append(Msg(*row))
+		count +=1
 
-# Iterate through each row in CSV file
-# But first store the header
-header_line = next(f)
-for row in f:
-	data_row = row.rstrip().split('\t')
-	data.append(data_row)
-	count += 1
-'''
-	if 
+legend = ['>100M<1G','>10M<100M','<100K','>1M<10M','>100K<1M']
+plt.hist([score_india, score_pk], color=['orange', 'green'])
+plt.xlabel("TTLB in ms (logarithmic scale base 2")
+plt.ylabel("% of TTLB times in sample")
+plt.legend(legend)
+plt.xticks(range(1, 33554432))
+plt.yticks(range(0, 14))
+plt.title('Time To Last Byte DistributionPer Object Size Class')
+plt.show()
 
-	elif
+print count	# debug to check that all rows are read	
 
-	else
-'''
-#print data
-print count
